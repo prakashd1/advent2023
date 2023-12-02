57321e8fa5a590f80f36ffb07c4f235cd0c4f7ff
prakash <prakash.dayaramani@gmail.com>
2023-12-02 19:34:26 +0530

    Added as new

---
+++ src/main/java/com/pd/advent/AdventApplication.java
@@ -0,0 +1,13 @@
+package com.pd.advent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdventApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pd/advent/AdventApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pd.advent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdventApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
