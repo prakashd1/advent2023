71fe12eb8dcb43f6e5794cbb208b7a51136dc126
prakash <prakash.dayaramani@gmail.com>
2023-12-09 11:33:30 +0530

    Added day 9

---
+++ src/main/java/com/pd/advent/day9/Mirage.java
@@ -0,0 +1,9 @@
+package com.pd.advent.day9;
+
+
+
+public class Mirage{
+    Mirage parent;
+    long arr[];
+    Mirage child;
+}
---
+++ src/main/java/com/pd/advent/day9/MirageMaintenance02.java
@@ -0,0 +1,63 @@
+package com.pd.advent.day9;
+
+import com.pd.advent.InputUtil;
+
+import java.io.BufferedReader;
+
+
+
+public class MirageMaintenance02 {
+    public static void main(String[] args) throws Exception{
+        MirageMaintenance02 s=new MirageMaintenance02();
+
+        long output;
+        try (BufferedReader reader = InputUtil.readInput("./src/main/resources/day9/input.txt")) {
+
+            String line;
+            output = 0;
+
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+                output+=s.processLineAndCalculate(line);
+
+            }
+        }
+        System.out.println(output);
+    }
+
+    private long processLineAndCalculate(String line) {
+        long result=0;
+        String []lines = line.split("\\s");
+        long []first = new long[lines.length];
+        for (int i=0;i<lines.length;i++){
+            first[i] = Long.parseLong(lines[i]);
+        }
+        Mirage start = new Mirage();
+        start.arr = first;
+        Mirage curr=start;
+        while(!allZeros(curr.arr)){
+            Mirage next = new Mirage();
+            next.arr = new long[curr.arr.length-1];
+            next.parent=curr;
+            curr.child=next;
+            for(int i=0;i<next.arr.length;i++){
+                next.arr[i] = curr.arr[i+1] - curr.arr[i];
+            }
+            curr=curr.child;
+        }
+        long val = 0;
+        while(curr!=null){
+            val = curr.arr[0] -val;
+            curr=curr.parent;
+        }
+        return val;
+    }
+
+    public boolean allZeros(long []arr){
+        for(long i:arr){
+            if(i!=0) return false;
+        }
+        return true;
+    }
+}
